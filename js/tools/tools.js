7df6f5d8b5cf24af3852f33d147a4ee1de07fa1e
a7cc <1123511349@qq.com>
2023-11-10 15:36:32 +0800

    Site updated: 2023-11-10 15:36:29

---
+++ js/tools/tools.js
@@ -0,0 +1,171 @@
+$(function() {
+    var running = 0,
+        what = $("#foodwhat"),
+        btn = $("#foodstart"),
+        book = [
+            ["早上",
+                "面包 蛋糕 荷包蛋 烧饼 饽饽 油条 馄饨 火腿 面条 小笼包  玉米粥 肉包 山东煎饼 饺子 煎蛋 烧卖 生煎 锅贴 包子 酸奶 苹果 梨 香蕉 皮蛋瘦肉粥 蛋挞 南瓜粥 煎饼 玉米糊 泡面 粥 馒头 燕麦片 水煮蛋 米粉 豆浆 牛奶 花卷 豆腐脑 煎饼果子 小米粥 黑米糕 鸡蛋饼 牛奶布丁 水果沙拉 鸡蛋羹 南瓜馅饼 鸡蛋灌饼 奶香小馒头 汉堡包 披萨 八宝粥 三明治 豆沙红薯饼 驴肉火烧 粥 粢饭糕 蒸饺 白粥"
+            ],
+            ["中午",
+                "海南粉 抱罗粉 文昌鸡饭 火锅 炒粉 盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉 蛋包饭 粥"
+            ],
+            ["晚上",
+                "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉"
+            ]
+        ],
+        list = book[1][1];
+        times = 0,
+        timer = 0;
+
+    btn.val("开始").removeAttr("disabled").click(function() {
+        var l = list.replace(/ +/g, " ").replace(/^ | $/g, "").split(" ");
+        if (!running) {
+            times++;
+            if (times == 3) {
+                btn.val("继续");
+                return false;
+            }
+            $(this).val("停止");
+            timer = setInterval(function() {
+                var r = Math.ceil(Math.random() * l.length);
+                food = l[r - 1];
+                what.html(food);
+            }, 60);
+            running = 1;
+        } else {
+            btn.val("不行，换一个");
+            clearInterval(timer);
+            running = 0;
+        };
+    });
+
+    document.onkeydown = function(e) {
+        e = e || window.event;
+        if (e.keyCode == 32) $("#foodstart").trigger("click");
+    };
+});
+
+// 人民币大写转换器
+function convertCurrency(currencyDigits) {
+    // 常量
+        var MAXIMUM_NUMBER = 99999999999.99;
+        // 预定义输出的基数字符和货币符号
+        var CN_ZERO = "零";
+        var CN_ONE = "壹";
+        var CN_TWO = "贰";
+        var CN_THREE = "叁";
+        var CN_FOUR = "肆";
+        var CN_FIVE = "伍";
+        var CN_SIX = "陆";
+        var CN_SEVEN = "柒";
+        var CN_EIGHT = "捌";
+        var CN_NINE = "玖";
+        var CN_TEN = "拾";
+        var CN_HUNDRED = "佰";
+        var CN_THOUSAND = "仟";
+        var CN_TEN_THOUSAND = "万";
+        var CN_HUNDRED_MILLION = "亿";
+        var CN_SYMBOL = "";
+        var CN_DOLLAR = "元";
+        var CN_TEN_CENT = "角";
+        var CN_CENT = "分";
+        var CN_INTEGER = "整";
+    
+    // 变量
+        var integral;    // Represent integral part of digit number.
+        var decimal;    // Represent decimal part of digit number.
+        var outputCharacters;    // The output result.
+        var parts;
+        var digits, radices, bigRadices, decimals;
+        var zeroCount;
+        var i, p, d;
+        var quotient, modulus;
+    
+    // 验证输入字符串
+        currencyDigits = currencyDigits.toString();
+        if (currencyDigits == "") {
+            alert("Empty input!");
+            return "";
+        }
+        if (currencyDigits.match(/[^,.\d]/) != null) {
+            alert("Invalid characters in the input string!");
+            return "";
+        }
+        if ((currencyDigits).match(/^((\d{1,3}(,\d{3})*(.((\d{3},)*\d{1,3}))?)|(\d+(.\d+)?))$/) == null) {
+            alert("Illegal format of digit number!");
+            return "";
+        }
+    
+    // Normalize the format of input digits:
+        currencyDigits = currencyDigits.replace(/,/g, "");    // Remove comma delimiters.
+        currencyDigits = currencyDigits.replace(/^0+/, "");    // Trim zeros at the beginning.
+        // Assert the number is not greater than the maximum number.
+        if (Number(currencyDigits) > MAXIMUM_NUMBER) {
+            alert("Too large a number to convert!");
+            return "";
+        }
+    
+    // Process the coversion from currency digits to characters:
+        // Separate integral and decimal parts before processing coversion:
+        parts = currencyDigits.split(".");
+        if (parts.length > 1) {
+            integral = parts[0];
+            decimal = parts[1];
+            // Cut down redundant decimal digits that are after the second.
+            decimal = decimal.substr(0, 2);
+        }
+        else {
+            integral = parts[0];
+            decimal = "";
+        }
+        // Prepare the characters corresponding to the digits:
+        digits = new Array(CN_ZERO, CN_ONE, CN_TWO, CN_THREE, CN_FOUR, CN_FIVE, CN_SIX, CN_SEVEN, CN_EIGHT, CN_NINE);
+        radices = new Array("", CN_TEN, CN_HUNDRED, CN_THOUSAND);
+        bigRadices = new Array("", CN_TEN_THOUSAND, CN_HUNDRED_MILLION);
+        decimals = new Array(CN_TEN_CENT, CN_CENT);
+        // Start processing:
+        outputCharacters = "";
+        // Process integral part if it is larger than 0:
+        if (Number(integral) > 0) {
+            zeroCount = 0;
+            for (i = 0; i < integral.length; i++) {
+                p = integral.length - i - 1;
+                d = integral.substr(i, 1);
+                quotient = p / 4;
+                modulus = p % 4;
+                if (d == "0") {
+                    zeroCount++;
+                }
+                else {
+                    if (zeroCount > 0)
+                    {
+                        outputCharacters += digits[0];
+                    }
+                    zeroCount = 0;
+                    outputCharacters += digits[Number(d)] + radices[modulus];
+                }
+                if (modulus == 0 && zeroCount < 4) {
+                    outputCharacters += bigRadices[quotient];
+                }
+            }
+            outputCharacters += CN_DOLLAR;
+        }
+        // Process decimal part if there is:
+        if (decimal != "") {
+            for (i = 0; i < decimal.length; i++) {
+                d = decimal.substr(i, 1);
+                if (d != "0") {
+                    outputCharacters += digits[Number(d)] + decimals[i];
+                }
+            }
+        }
+        // Confirm and return the final output string:
+        if (outputCharacters == "") {
+            outputCharacters = CN_ZERO + CN_DOLLAR;
+        }
+        if (decimal == "") {
+            outputCharacters += CN_INTEGER;
+        }
+        outputCharacters = CN_SYMBOL + outputCharacters;
+        return outputCharacters;
+    }
